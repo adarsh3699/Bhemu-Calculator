435c09a1ee6312b681e7a93b27969edeea23212b
adarsh3699 <adarsh3699@gmail.com>
2024-04-11 23:58:39 +0530

    create commponent

--- src/App.js
+++ src/App.js
@@ -1,79 +1,18 @@
 import React, { useCallback, useState } from 'react';
-
+import Calculator from './components/calculator/Calculator';
 import './styles/App.css';
 
 function App() {
 	const [theme, setTheme] = useState('dark');
-	const [value, setValue] = useState('');
-
 	const toggleTheme = useCallback(() => {
 		setTheme(theme === 'dark' ? 'light' : 'dark');
 	}, [theme]);
 
-	const handleBrnClick = useCallback(
-		(e) => {
-			setValue(value + e.target.innerHTML);
-		},
-		[value]
-	);
-
-	const handleInputChange = useCallback((e) => {
-		if (!isNaN(Number(e.nativeEvent.data)) || ['+', '-', '*', '/', '.'].includes(e.nativeEvent.data)) {
-			setValue(e.target.value);
-		}
-	}, []);
-
-	const handleEqualToBtn = useCallback(() => {
-		try {
-			// eslint-disable-next-line no-eval
-			const result = eval(value);
-			console.log(result);
-			if (String(result).split('.')[1].length > 12) {
-				setValue(result.toFixed(3).replace(/\.?0*$/, ''));
-			} else {
-				setValue(result);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	}, [value]);
-
-	const handleClearBtn = useCallback(() => {
-		setValue('');
-	}, []);
-
 	return (
 		<div className={'background ' + theme}>
 			<div className="toggleBtn" onClick={toggleTheme}></div>
-			<div className="calculator">
-				<div className="buttons">
-					<input type="text" id="value" value={value} onChange={handleInputChange} />
-					<span onClick={handleClearBtn} id="clear">
-						Clear
-					</span>
-					<span onClick={handleBrnClick}>/</span>
-					<span onClick={handleBrnClick}>*</span>
-					<span onClick={handleBrnClick}>7</span>
-					<span onClick={handleBrnClick}>8</span>
-					<span onClick={handleBrnClick}>9</span>
-					<span onClick={handleBrnClick}>-</span>
-					<span onClick={handleBrnClick}>4</span>
-					<span onClick={handleBrnClick}>5</span>
-					<span onClick={handleBrnClick}>6</span>
-					<span onClick={handleBrnClick} id="plus">
-						+
-					</span>
-					<span onClick={handleBrnClick}>1</span>
-					<span onClick={handleBrnClick}>2</span>
-					<span onClick={handleBrnClick}>3</span>
-					<span onClick={handleBrnClick}>0</span>
-					<span onClick={handleBrnClick}>00</span>
-					<span onClick={handleBrnClick}>.</span>
-					<span onClick={handleEqualToBtn} id="equal">
-						=
-					</span>
-				</div>
-			</div>
+
+			<Calculator />
 		</div>
 	);
 }
---
+++ src/components/calculator/Calculator.js
@@ -0,0 +1,70 @@
+import React, { useCallback, useState } from 'react';
+import './calculator.css';
+
+function Calculator() {
+	const [value, setValue] = useState('');
+
+	const handleBrnClick = useCallback(
+		(e) => {
+			setValue(value + e.target.innerHTML);
+		},
+		[value]
+	);
+
+	const handleInputChange = useCallback((e) => {
+		if (!isNaN(Number(e.nativeEvent.data)) || ['+', '-', '*', '/', '.'].includes(e.nativeEvent.data)) {
+			setValue(e.target.value);
+		}
+	}, []);
+
+	const handleEqualToBtn = useCallback(() => {
+		try {
+			// eslint-disable-next-line no-eval
+			const result = eval(value);
+			if (String(result).split('.')[1]?.length > 12) {
+				setValue(result.toFixed(3).replace(/\.?0*$/, ''));
+			} else {
+				setValue(result);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}, [value]);
+
+	const handleClearBtn = useCallback(() => {
+		setValue('');
+	}, []);
+	return (
+		<div className="calculator">
+			<div className="buttons">
+				<input type="text" id="value" value={value} onChange={handleInputChange} />
+				<span onClick={handleClearBtn} id="clear">
+					Clear
+				</span>
+				<span onClick={handleBrnClick}>/</span>
+				<span onClick={handleBrnClick}>*</span>
+				<span onClick={handleBrnClick}>7</span>
+				<span onClick={handleBrnClick}>8</span>
+				<span onClick={handleBrnClick}>9</span>
+				<span onClick={handleBrnClick}>-</span>
+				<span onClick={handleBrnClick}>4</span>
+				<span onClick={handleBrnClick}>5</span>
+				<span onClick={handleBrnClick}>6</span>
+				<span onClick={handleBrnClick} id="plus">
+					+
+				</span>
+				<span onClick={handleBrnClick}>1</span>
+				<span onClick={handleBrnClick}>2</span>
+				<span onClick={handleBrnClick}>3</span>
+				<span onClick={handleBrnClick}>0</span>
+				<span onClick={handleBrnClick}>00</span>
+				<span onClick={handleBrnClick}>.</span>
+				<span onClick={handleEqualToBtn} id="equal">
+					=
+				</span>
+			</div>
+		</div>
+	);
+}
+
+export default Calculator;
