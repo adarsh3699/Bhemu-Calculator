18366dbee92b6c57d1e8caf62ba9ae1a258e208b
adarsh3699 <adarsh3699@gmail.com>
2024-07-03 23:44:05 +0530

    feat: Add STD Calculator screen component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
 import React, { useState } from 'react';
-import MenuBar from './components/menuBar/MenuBar';
+import { useSearchParams } from 'react-router-dom';
+
 import NavBar from './components/navBar/NavBar';
-import Calculator from './components/calculator/Calculator';
+import MenuBar from './components/menuBar/MenuBar';
+
+import Calculator from './components/screen/calculator/Calculator';
+import STDcalc from './components/screen/stdCalc/STDcalc';
 
 import './styles/App.css';
 
+const menuItems = [
+	{
+		name: 'Calculator',
+		menuName: 'calculator',
+		screen: Calculator,
+	},
+	{
+		name: 'STD Calculator',
+		menuName: 'std-calculator',
+		screen: STDcalc,
+	},
+	{
+		name: 'Matrix determinant calculator',
+		menuName: 'matrix-determinant-calculator',
+		// screen: MatrixDeterminantCalculator,
+	},
+	{
+		name: 'Number Patterns Generator',
+		menuName: 'number-patterns-generator',
+	},
+	{
+		name: 'Prime Number checker',
+		menuName: 'prime-number-checker',
+	},
+	{
+		name: 'Binary No. to decimal No',
+		menuName: 'binary-no-to-decimal-no',
+	},
+];
+
 function App() {
 	const [isDarkMode, setDarkMode] = useState(true);
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	const menuName = searchParams.get('menu') || 'calculator';
 
 	return (
 		<div className={'background ' + (isDarkMode ? 'dark' : 'light')}>
 			<NavBar setDarkMode={setDarkMode} />
 			<div className="pageContains">
-				<MenuBar />
-				<Calculator />
+				<MenuBar menuItems={menuItems} setSearchParams={setSearchParams} menuName={menuName} />
+
+				{menuItems.map((item, index) => (
+					<React.Fragment key={index}>{item.menuName === menuName && <item.screen />}</React.Fragment>
+				))}
 			</div>
 		</div>
 	);
--- src/components/menuBar/MenuBar.js
+++ src/components/menuBar/MenuBar.js
@@ -1,54 +1,21 @@
 import React, { useCallback } from 'react';
-import { useSearchParams } from 'react-router-dom';
-
 import './menuBar.css';
 
-const menuItems = [
-	{
-		name: 'Calculator',
-		folderName: 'calculator',
-	},
-	{
-		name: 'STD Calculator',
-		folderName: 'std-calculator',
-	},
-	{
-		name: 'Matrix determinant calculator',
-		folderName: 'matrix-determinant-calculator',
-	},
-	{
-		name: 'Number Patterns Generator',
-		folderName: 'number-patterns-generator',
-	},
-	{
-		name: 'Prime Number checker',
-		folderName: 'prime-number-checker',
-	},
-	{
-		name: 'Binary No. to decimal No',
-		folderName: 'binary-no-to-decimal-no',
-	},
-];
-
-function MenuBar() {
-	const [searchParams, setSearchParams] = useSearchParams();
-	// const navigate = useNavigate();
-	const folderName = searchParams.get('folder');
-
+function MenuBar({ menuItems, setSearchParams, menuName }) {
 	const handleMenuChange = useCallback(
 		(item) => {
-			if (folderName === item?.folderName) return;
+			if (menuName === item?.menuName) return;
 
-			setSearchParams({ folder: item?.folderName });
+			setSearchParams({ menu: item?.menuName });
 		},
-		[folderName, setSearchParams]
+		[menuName, setSearchParams]
 	);
 	return (
 		<div className="menuBar">
 			{menuItems.map((item, index) => (
 				<div
 					key={index}
-					className={`menuBtn ${folderName === item.folderName ? 'activeMenu' : ''}`}
+					className={`menuBtn ${menuName === item.menuName ? 'activeMenu' : ''}`}
 					onClick={() => handleMenuChange(item)}
 				>
 					{item.name}
