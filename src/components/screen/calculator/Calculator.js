a6955792259fc659f1cfb9eaa7a23595e32f3d2c
adarsh3699 <adarsh3699@gmail.com>
2024-07-11 00:04:59 +0530

    chore: Add hide button to calculator history section

--- src/components/screen/calculator/Calculator.js
+++ src/components/screen/calculator/Calculator.js
@@ -71,6 +71,38 @@ function Calculator() {
 				</div>
 			</div>
 			<div className="calcHistory right">
+				<button className="arrow" type="button" aria-label="Hide">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 13 96"
+						width="13"
+						height="96"
+						fill="none"
+						className="IrLwCg"
+					>
+						<path className="arowCurve" d="M0,0 h1 c0,20,12,12,12,32 v32 c0,20,-12,12,-12,32 H0 z"></path>
+						<path className="arowBorder" d="M0.5,0 c0,20,12,12,12,32 v32 c0,20,-12,12,-12,32"></path>
+					</svg>
+					<div className="arrowIconDiv">
+						<span aria-hidden="true" className="R3BUpw dkWypw RMcv3A lmfTqA">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="12"
+								height="12"
+								viewBox="0 0 12 12"
+								fill="none"
+							>
+								<path
+									stroke="currentColor"
+									strokeLinecap="round"
+									strokeWidth="1.25"
+									d="M7 3.17 4.88 5.3a1 1 0 0 0 0 1.42L7 8.83"
+								></path>
+							</svg>
+						</span>
+					</div>
+				</button>
+
 				<div className="historyTitle">
 					<h1>History</h1>
 					<div className="clearIcon" onClick={() => setHistory([])}></div>
